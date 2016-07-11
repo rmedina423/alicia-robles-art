af9a3ea7334e5b546ac33801be55b0386f12d696
Ryan M. Medina <rmedina423@gmail.com>
2016-07-10 22:19:29 -0700

    remove document ready function

--- _site/js/fancybox.js
+++ _site/js/fancybox.js
@@ -1,11 +1,9 @@
-$(document).ready(function () {
-	var browserVW = $( window ).width();
+var browserVW = $( window ).width();
 
-	if (browserVW >= 600) {
-		$(".fancybox").fancybox();
-	} else {
-		$('.fancybox').on('click', function (event) {
-			event.preventDefault();
-		})
-	}
-});
+if (browserVW >= 600) {
+	$(".fancybox").fancybox();
+} else {
+	$('.fancybox').on('click', function (event) {
+		event.preventDefault();
+	})
+}
