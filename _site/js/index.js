4e357aa77a5127200a9c6300b33e264b057cf46a
Ryan M. Medina <rmedina423@gmail.com>
2016-07-10 22:32:02 -0700

    extract #targetPosition & #tabOnClick

--- _site/js/index.js
+++ _site/js/index.js
@@ -1,30 +1,32 @@
+function targetPosition(element) {
+
+	var $targetClass = $(element.attr('href'))
+	var elementPosition = $targetClass.offset().top
+	var marginTop = $targetClass.css('margin-top').replace(/[^\d]/g, "")
+	var parsedMargin = parseInt(marginTop)
+
+	return elementPosition - parsedMargin
+}
+
+function tabOnClick(event) {
+	event.preventDefault()
+
+	var $this = $(this)
+
+	$('html, body').animate({
+		scrollTop: targetPosition($this)
+	}, 500);
+}
+
 $(document).ready(function () {
 
-	function targetPosition(element) {
-
-		var $targetClass = $(element.attr('href'))
-		var elementPosition = $targetClass.offset().top
-		var marginTop = $targetClass.css('margin-top').replace(/[^\d]/g, "")
-		var parsedMargin = parseInt(marginTop)
-
-		return elementPosition - parsedMargin
-	}
-
-	$('.primary-tabs a').on('click', function (event) {
-		event.preventDefault()
-
-		var $this = $(this)
-
-		$('html, body').animate({
-			scrollTop: targetPosition($this)
-		}, 500);
-	})
+	$('.primary-tabs a').on('click', tabOnClick)
 
 	// init Masonry
 	var $portfolioContent = $('.portfolio .content').masonry();
 
 	// layout Masonry after each image loads
-	$portfolioContent.imagesLoaded().progress( function () {
+	$portfolioContent.imagesLoaded().progress(function () {
 		$portfolioContent.masonry('layout');
 	});
 
