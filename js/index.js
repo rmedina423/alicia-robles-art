e5852ed462b38e6db0b753aed2123f0edd65a428
Ryan M. Medina <rmedina423@gmail.com>
2016-07-10 20:23:41 -0700

    Initial dist subtree commit

---
+++ js/index.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+
+	function targetPosition(element) {
+
+		var $targetClass = $(element.attr('href'))
+		var elementPosition = $targetClass.offset().top
+		var marginTop = $targetClass.css('margin-top').replace(/[^\d]/g, "")
+		var parsedMargin = parseInt(marginTop)
+
+		return elementPosition - parsedMargin
+	}
+
+	$('.primary-tabs a').on('click', function(event) {
+		event.preventDefault()
+
+		var $this = $(this)
+
+		$('html, body').animate({
+			scrollTop: targetPosition($this)
+		}, 500);
+
+	})
+
+	// init Masonry
+	var $portfolioContent = $('.portfolio .content').masonry();
+
+	// layout Masonry after each image loads
+	$portfolioContent.imagesLoaded().progress( function() {
+		$portfolioContent.masonry('layout');
+	});
+
+	var $contactForm = $('#contact-form');
+
+	$contactForm.submit(function(event) {
+		event.preventDefault();
+
+		$.ajax({
+			url: '//formspree.io/aliciaroblesart@gmail.com',
+			method: 'POST',
+			data: $(this).serialize(),
+			dataType: 'json',
+			beforeSend: function() {
+				$contactForm[0].reset();
+				$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+			},
+			success: function(data) {
+				$contactForm.find('.alert--loading').hide();
+				$contactForm.append('<div class="alert alert--success">Message sent!</div>');
+
+				setTimeout(function () {
+					$('.alert').fadeOut();
+				}, 5000);
+			},
+			error: function(err) {
+				$contactForm.find('.alert--loading').hide();
+				$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+			}
+		});
+	});
+
+})
